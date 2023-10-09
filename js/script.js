5d25d94ed02ef973dd2eabbe0a7acddd2176f024
Mathzzuco <matheusmazzuco05@gmail.com>
2023-10-09 20:12:31 -0300

    change grid

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
-const container = document.querySelector("#container");
+const grid = document.querySelector(".grid");
 
-let userInput = 16;
-let numOfIterations = userInput * userInput;
+const userInput = 16;
+const numOfIterations = userInput * userInput;
+const gridSize = 600;
+const squareSize = gridSize / userInput;
 
 for (x = 0; x < numOfIterations; x++) {
-  const drawBox = document.createElement("div");
+  let drawBox = document.createElement("div");
   drawBox.classList.add("draw-box");
-  container.appendChild(drawBox);
+
+  drawBox.style.width = squareSize + "px";
+  drawBox.style.height = squareSize + "px";
+
+  grid.appendChild(drawBox);
+  let drawBoxAll = document.querySelectorAll(".draw-box");
+  drawBoxAll[x].addEventListener("mouseover", mouseOver);
+  drawBoxAll[x].addEventListener("mouseout", mouseOut);
+}
+
+// drawBox = document.querySelectorAll(".draw-box");
+
+// document.querySelector(".grid div").addEventListener("mouseover", mouseOver);
+// document.querySelector(".grid div").addEventListener("mouseout", mouseOut);
+
+function mouseOver(e) {
+  const element = e.target;
+  element.style.background = "red";
+}
+
+function mouseOut(e) {
+  const element = e.target;
+  element.style.background = "white";
 }
